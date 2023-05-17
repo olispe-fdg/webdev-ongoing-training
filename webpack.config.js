7ad2bc273edd44a1a6968558ab1e5c16ffcb2a07
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2023-05-17 10:40:58 +0100

    Set up webpack with sass

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.html$/i,
+				use: "html-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+			filename: "index.html",
+			inject: false,
+		}),
+	],
+};
